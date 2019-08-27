7e0a5af32d143b20072058255bd7f7a47aa0277c
FFengIll <fengyouzheng@gmail.com>
2019-08-27 12:57:11 +0800

    update snapshot: - add debug mode - promote log - use pickle for key and value - logic update - update tests

--- utils3/data/snapshot.py
+++ utils3/data/snapshot.py
@@ -5,7 +5,7 @@ from plyvel import DB
 from logbook import StreamHandler, Logger
 import logging
 
-handler = StreamHandler(sys.stdout, level='DEBUG')
+handler = StreamHandler(sys.stdout, level='WARNING')
 handler.push_application()
 logger = Logger('data.snapshot')
 
@@ -17,7 +17,7 @@ class Snapshot(object):
     so we can bypass the known pair to save time/cpu/...
     """
 
-    def __init__(self, dbpath, *args, **kwargs):
+    def __init__(self, dbpath, *args, debug=False, **kwargs):
         super().__init__(*args, **kwargs)
         try:
             self.db = DB(dbpath, create_if_missing=True)
@@ -26,6 +26,9 @@ class Snapshot(object):
             raise e
         self.old_key = None
         self.upgrade = False
+
+        if debug:
+            handler.level = logging.DEBUG
 
     def __del__(self):
         self.close()
@@ -52,67 +55,50 @@ class Snapshot(object):
         support all basic type.
         but never support recursion data, like List[Dict].
         all data will be translated to bytes if possible.
-        FIXME: now obey json as much as possible, but we may need pickle
+
+        use pickle to save bytes so we can store any possible data.
         """
         s = pickle.dumps(data)
         return s
-
-        if isinstance(data, bytes):
-            return data
-        elif isinstance(data, str):
-            return data.encode()
-        elif isinstance(data, list) or isinstance(data, dict):
-            data = json.dumps(data)
-            data = data.encode()
-            return data
-        elif isinstance(data, tuple):
-            data = json.dumps(list(data))
-            data = data.encode()
-            return data
-        elif isinstance(data, int):
-            return str(data).encode()
-        elif isinstance(data, bool):
-            return bytes(data)
-        elif data is None:
-            # FIXME: how to process `None`
-            data = json.dumps([])
-            data = data.encode()
-            return data
 
     @staticmethod
     def recover_bytes(data):
         s = data
         return pickle.loads(s)
 
-        data = data.decode()
-        try:
-            return json.loads(data)
-        except:
-            return data
-
-    def get(self, k, default=None):
+    def get(self, key, default=None):
         """
-        user shold determine the key exist or not 
+        user shold determine the key exist or not
         (according to the default)
         """
-        logger.debug('key: {}', k, )
+        logger.debug('key: {}', key, )
 
-        key = self.to_bytes(k)
-        res = self.db.get(key, default)
+        key = self.to_bytes(key)
+        data = self.db.get(key, default)
 
-        if res != default:
-            res = self.recover_bytes(res)
-            logger.debug('get exist: {} -> data(type={}, {})',
-                         k, type(res), res if isinstance(res, int) else len(res))
+        if data != default:
+            logger.debug('get exist: {} -> data(type={})', key, type(data))
 
-        return res
+        return data
+
+    def get_result(self, key) -> bytes:
+        """
+        get the value related to the key,
+        return the result by decoding it from bytes
+        :param key:
+        :return:
+        """
+        data = self.get(key)
+        if data is None:
+            return None
+        else:
+            return self.recover_bytes(data)
 
     def put(self, k, v):
-        logger.debug('put: {} -> data(type={}, size={})',
-                     k, type(v), v if isinstance(v, int) else len(v))
+        logger.debug('put: {} -> data(type={})', k, type(v))
         key = self.to_bytes(k)
-        value = self.to_bytes(v)
-        return self.db.put(key, value)
+        data = self.to_bytes(v)
+        return self.db.put(key, data)
 
     def exist(self, key):
         return key in self
@@ -138,7 +124,7 @@ class Snapshot(object):
 
     def get_key(self, positions, keys, *args, **kwargs):
         logger.debug('get key from {} {} (positions:{} keys:{})',
-                     args, kwargs, positions, keys,)
+                     args, kwargs, positions, keys, )
 
         key = []
         for p in positions:
@@ -147,12 +133,12 @@ class Snapshot(object):
             key.append(kwargs[k])
         return key
 
-    def snapshot(self, *_args, ignore=None,  redos=None, **_kwargs):
+    def snapshot(self, *_args, ignore=None, redos=None):
         """
         the args:
         can be number: the idx/pos of given args
         can be string: the key name in kwargs
-        
+
         the kwargs:
         some config for snapshot
         """
@@ -163,7 +149,6 @@ class Snapshot(object):
         _ignore = ignore
         # will redo for some return value, should be a list
         _redos = redos or []
-        
 
         logger.debug('choose position args: {}', positions)
         logger.debug('choose name kwargs: {}', keys)
@@ -179,6 +164,7 @@ class Snapshot(object):
                     logger.info('will upgrade old_key: {}', old_key)
                     result = self.get(old_key)
                     if result is not None:
+                        result = self.recover_bytes(result)
                         logger.info('upgrade result: {} -> {} -> {}',
                                     old_key, key, result)
                         self.delete(old_key)
@@ -188,25 +174,27 @@ class Snapshot(object):
                     result = self.get(key)
                     if result is None:
                         pass
-                    elif result in _redos:
-                        logger.warning('redo result')
-                        logging.getLogger().warning('redo result')
                     else:
-                        return result
+                        result = self.recover_bytes(result)
+
+                        if result in _redos:
+                            logger.warning('redo result: {}', result)
+                            logging.getLogger().warning('redo result')
+                        else:
+                            return result
 
                 result = func(*args, **kwargs)
-
                 value = result
-                if value != _ignore and value not in _redos:
-                    self.put(key, value)
-                else:
-                    logger.warning('ignore result')
 
+                if value == _ignore:
+                    logger.warning('ignore result: {}', result)
+                elif value in _redos:
+                    logger.warning('redo result: {}', result)
+                else:
+                    self.put(key, value)
 
                 return result
 
             return inner
 
         return do_snapshot
-
-
--- utils3/tests/test_data.py
+++ utils3/tests/test_data.py
@@ -4,18 +4,19 @@ import pytest
 import logging
 
 
-def test_kv():
-    app = Snapshot('./db/kv')
+def test_kv(tmpdir):
+    path = tmpdir.mkdir('kv')
+    app = Snapshot(str(path))
 
     # old
     @app.snapshot(1, 2, 'info')
     def inner(a, b, c, **kwargs):
         print(kwargs)
-        return a+b+c
+        return a + b + c
 
     res = inner(5, 6, 1, info='test', extra=None)
 
-    assert res == app.get([6, 1, 'test'])
+    assert res == app.get_result([6, 1, 'test'])
 
     # new
     app.set_upgrade(1, 2, 'info')
@@ -23,50 +24,58 @@ def test_kv():
     @app.snapshot(1, 2, 'info', 'extra')
     def inner(a, b, c, **kwargs):
         print(kwargs)
-        return a+b+c
+        return a + b + c
+
     res = inner(5, 6, 1, info='test', extra=None)
 
-    assert res == app.get([6, 1, 'test', None])
+    assert res == app.get_result([6, 1, 'test', None])
 
 
-def test_null():
-    app = Snapshot('./db/null')
+def test_null(tmpdir):
+    path = tmpdir.mkdir('null')
+    app = Snapshot(str(path))
 
     @app.snapshot(1, 2, 'info')
     def inner(a, b, c, **kwargs):
         print(kwargs)
         return []
+
     res = inner(5, 6, 1, info='test', extra=None)
-    db_value = app.get([6, 1, 'test'])
+    db_value = app.get_result([6, 1, 'test'])
     print(res)
     print(db_value)
     assert res == db_value
 
 
-def test_invalid():
-    app = Snapshot('./db/invalid')
+def test_invalid(tmpdir):
+    path = tmpdir.mkdir('invalid')
+    app = Snapshot(str(path))
 
-    @app.snapshot(1, 2, 'info', invalid=[])
+    @app.snapshot(1, 2, 'info', ignore=[])
     def inner(a, b, c, **kwargs):
         print(kwargs)
         return []
+
     res = inner(5, 6, 1, info='test', extra=None)
-    db_value = app.get([6, 1, 'test'], default=Exception)
-    assert db_value == Exception
+    db_value = app.get_result([6, 1, 'test'])
+    assert db_value is None
 
 
-def test_redo(caplog):
+def test_redo(tmpdir, caplog):
     # with caplog.at_level(logging.WARNING, logger="data.snapshot"):
 
-    app = Snapshot('./db/redo')
+    path = tmpdir.mkdir('redo')
+    app = Snapshot(str(path))
 
     @app.snapshot(1, 2, 'info', redos=[[]])
     def inner(a, b, c, **kwargs):
         print(kwargs)
         return []
 
-    res = inner(5, 6, 1, info='test', extra=None)
-    db_value = app.get([6, 1, 'test'], default=Exception)
+    db_value = app.put([6, 1, 'test'], [])
+    db_value = app.get_result([6, 1, 'test'])
+    assert db_value == []
+
     res = inner(5, 6, 1, info='test', extra=None)
 
     redo = False
@@ -76,28 +85,30 @@ def test_redo(caplog):
     assert redo == True
 
 
-def test_iter():
-    app = Snapshot('./db/iter')
+def test_iter(tmpdir):
+    path = tmpdir.mkdir('iter')
+    app = Snapshot(str(path))
 
     for k, v in app:
         print(k, v)
 
 
-def test_op():
-    app = Snapshot('./db/op')
+def test_op(tmpdir):
+    path = tmpdir.mkdir('op')
+    app = Snapshot(str(path))
 
     key = ['test_put']
 
-    app.get(key)
+    app.get_result(key)
 
     right = ['test_put']
     app.put(key, right)
-    left = app.get(key)
+    left = app.get_result(key)
     assert left == right
 
     right = 'test_put'
     app.put(key, 'test_put')
-    left = app.get(key)
+    left = app.get_result(key)
     assert left == right
 
     print(app.exist(key))
