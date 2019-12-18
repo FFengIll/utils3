deaabcc29819b306a3ea39e9af6f0c5d71b4189e
FFengIll <fengyouzheng@gmail.com>
2019-12-18 19:21:50 +0800

    update logger.

--- utils3/cli/logger.py
+++ utils3/cli/logger.py
@@ -1,19 +1,44 @@
 import logging
 import sys
+import os
 
 
-def get_logbook_logger():
-    from logbook import Logger, StreamHandler
+def get_logbook(name, level=logging.INFO, file=None):
+    import logbook
+    from logbook import StreamHandler
+    from logbook.more import ColorizedStderrHandler
 
-    # use string level
-    handler = StreamHandler(sys.stdout, level='INFO')
+    def log_type(record, handler):
+        log = "[{date}] [{level}] [{filename}] [{func_name}] [{lineno}] {msg}".format(
+            date=record.time,  # 日志时间
+            level=record.level_name,  # 日志等级
+            filename=os.path.split(record.filename)[-1],  # 文件名
+            func_name=record.func_name,  # 函数名
+            lineno=record.lineno,  # 行号
+            msg=record.message  # 日志内容
+        )
+        return log
+
+    if isinstance(level, str):
+        # logbook use different define
+        level = getattr(logbook, level.upper())
+
+    handler = logbook.StreamHandler(sys.stdout)
+    handler.formatter = log_type
     handler.push_application()
-    logger = Logger('cutwhite')
+
+    logger = logbook.Logger(name)
+    logger.handlers = []
+    logger.handlers.append(handler)
+    # set level on logger is enough
+    logger.level = level
+    return logger
 
 
 def get_color_logger(name, level=logging.NOTSET):
     try:
-        import colorlog, logging
+        import colorlog
+        import logging
         pass
     except:
         return get_logger(name, level)
@@ -28,19 +53,12 @@ def get_colored_logger(name, level=logging.NOTSET):
     # By default the install() function installs a handler on the root logger,
     # this means that log messages from your code and log messages from the
     # libraries that you use will all show up on the terminal.
-    coloredlogs.install(level=level)
+    # coloredlogs.install(level=level)
 
     # If you don't want to see log messages from libraries, you can pass a
     # specific logger object to the install() function. In this case only log
     # messages originating from that logger will show up on the terminal.
     coloredlogs.install(level=level, logger=logger)
-
-    # Some examples.
-    logger.debug("this is a debugging message")
-    logger.info("this is an informational message")
-    logger.warning("this is a warning message")
-    logger.error("this is an error message")
-    logger.critical("this is a critical message")
 
     return logger
 
@@ -54,7 +72,8 @@ def get_logger(name, level=logging.NOTSET, file=None):
         app_log = logging.getLogger(name)
 
         # set format        
-        format_str = logging.Formatter("%(asctime)s %(filename)s[%(lineno)d] %(levelname)s %(message)s")
+        format_str = logging.Formatter(
+            "%(asctime)s %(filename)s[%(lineno)d] %(levelname)s %(message)s")
 
         if len(app_log.handlers) <= 0:
 
@@ -80,7 +99,14 @@ def get_logger(name, level=logging.NOTSET, file=None):
 
 
 def test_get_colored_logger():
-    get_colored_logger(__file__, 'Debug')
+    logger = get_colored_logger(__file__, 'Debug')
+
+    # Some examples.
+    logger.debug("this is a debugging message")
+    logger.info("this is an informational message")
+    logger.warning("this is a warning message")
+    logger.error("this is an error message")
+    logger.critical("this is a critical message")
 
 
 if __name__ == '__main__':
